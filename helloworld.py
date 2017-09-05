e60233f7234a50a68282e470d48cf4e84d2e75e6
lucianocorreia <luciano.correia@gmail.com>
2017-09-05 16:20:47 -0300

    Init

--- helloworld.py
+++ helloworld.py
@@ -10,6 +10,12 @@ nome = "Luciano" if 1 > 2 else "Correia"
 # Lists
 names = ["Luciano", "Teixeira", "Correia"]
 names.append("Salvador")
+del names[3]
 existe = "Luciano" in names
 len(names) == 4
+
+# print(names[1:])  # ['Teixeira', 'Correia']
+# print(names[1:-1])  # ['Teixeira']
+
+# Loops
 
