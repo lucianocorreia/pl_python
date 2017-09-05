abd8fefa1b0d2ea0f2033a440f4e7da917c03190
lucianocorreia <luciano.correia@gmail.com>
2017-09-05 14:56:52 -0300

    Init

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,15 @@
 
 # String format
 name = "Hello {0}".format("Luciano")
-print(name)
+#print(name)
+
+# if
+nome = "Luciano" if 1 > 2 else "Correia"
+#print(nome)
+
+# Lists
+names = ["Luciano", "Teixeira", "Correia"]
+names.append("Salvador")
+existe = "Luciano" in names
+len(names) == 4
+
